180db8963f1fc80edc2378d29f50105121d2cf38
Michael Pierce <m.edward.pierce@gmail.com>
2018-01-11 20:24:00 -0800

    gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,64 @@
+// gulpfile
+require( 'dotenv' ).config()
+
+const gulp = require( 'gulp' )
+const concat = require( 'gulp-concat' )
+
+const exec = require( 'child_process' ).exec
+const fs = require( 'fs' )
+const path = require( 'path' )
+
+
+// Command line call wrapped in a promise
+function pexec( command ) {
+  return new Promise( ( resolve, reject ) => {
+    exec( command, ( err, stdout, stderr ) => {
+      if ( err ) {
+        reject( err )
+      }
+      console.log( stdout );
+      console.log( stderr );
+      resolve( stdout );
+    } )
+  } )
+}
+
+// initiate remote environment, install node, setup certbot, and nginx
+const initiateRemote = async () => {
+
+
+}
+
+// if build or zip folder, remove
+const clean = async () => {
+  if ( fs.existsSync( './zip' ) ) pexec( 'rm -rf ./zip' )
+  if ( fs.existsSync( './build' ) ) return pexec( 'rm -rf ./build && mkdir ./build' )
+  return fs.mkdirSync( './build' )
+}
+
+// compile app put in build folder, zip
+const compileProdServer = async () => {
+  return pexec( 'node_modules/.bin/browserify --entry lib/index.js --node --exclude utf-8-validate --exclude bufferutil --exclude forever > build/bundle.js' )
+}
+
+const compileClient = async () => {
+  return pexec( 'mkdir build/public && node_modules/.bin/uglifyjs lib/public/client.js > build/public/client.js && cp lib/public/style.css build/public' )
+}
+
+// zip build folder
+const zipDir = async () => {
+  // get latest commit
+  console.log( process.env.HOST)
+  let commit = await pexec( 'git rev-parse HEAD' )
+  return pexec( `mkdir zip && zip -r zip/${ commit.slice(0, 7) }.zip build `)
+
+}
+
+// push app to remote, unzip and deploy
+
+
+
+
+
+gulp.task( 'initiateRemote', initiateRemote )
+gulp.task( 'default', gulp.series( clean, gulp.parallel( [ compileProdServer, compileClient ] ), zipDir ) )
